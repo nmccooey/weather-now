0893db4ec45a69984ba1012f95de7f508ceed1f5
Nicholas McCooey <nicholasmccooey@gmail.com>
2020-11-06 08:24:01 -0500

    Added Header component and react-router-bootstrap.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from "react";
+import Header from "./components/Header";
+import { BrowserRouter as Router } from "react-router-dom";
 
 const App = () => {
-  return <div>Weather Now</div>;
+  return (
+    <Router>
+      <Header />
+      <main>Weather Now</main>
+    </Router>
+  );
 };
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { LinkContainer } from "react-router-bootstrap";
+import { Navbar, Nav, Container } from "react-bootstrap";
+
+const Header = () => {
+  return (
+    <header>
+      <Navbar className="bg-dark" variant="dark" expand="lg">
+        <Container>
+          <LinkContainer to="/">
+            <Navbar.Brand>Weather Now</Navbar.Brand>
+          </LinkContainer>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <LinkContainer to="/">
+                <Nav.Link>Home</Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/about">
+                <Nav.Link>About</Nav.Link>
+              </LinkContainer>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
+};
+
+export default Header;
