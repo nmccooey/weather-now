e9b705c7e2478f7aafadd82c6629e954af8f0068
Nicholas McCooey <nicholasmccooey@gmail.com>
2020-11-07 13:24:28 -0500

    Footer component.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
-import Header from "./components/Header";
 import { BrowserRouter as Router } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
     <Router>
       <Header />
       <main>Weather Now</main>
+      <Footer />
     </Router>
   );
 };
