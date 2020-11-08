d8b2ebefe760a88b18fd362cd783650348ddc92c
Nicholas McCooey <nicholasmccooey@gmail.com>
2020-11-08 08:16:06 -0500

    About component and added routing with BrowserRouter.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Container } from "react-bootstrap";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import About from "./components/About";
 
 const App = () => {
   return (
     <Router>
       <Header />
-      <main>Weather Now</main>
+      <main className="py-2">
+        <Container>
+          <Route path="/about" component={About} exact />
+        </Container>
+      </main>
       <Footer />
     </Router>
   );
