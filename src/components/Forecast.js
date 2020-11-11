af4186c980e85081914f2bda21187da4b6b1d19b
Nicholas McCooey <nicholasmccooey@gmail.com>
2020-11-11 08:45:28 -0500

    Added Loader component and UI for forecast with React-Bootstrap CardGroup.

--- src/components/Forecast.js
+++ src/components/Forecast.js
@@ -1,14 +1,65 @@
 import React from "react";
-import { Jumbotron, Container } from "react-bootstrap";
+import { Jumbotron, Container, Card, CardGroup } from "react-bootstrap";
 
 const Home = () => {
   return (
-    <Jumbotron className="mt-4">
+    <>
+      <Jumbotron className="mt-4">
+        <Container>
+          <h1>Your 3 Day Forecast</h1>
+          <p>Showing current weather conditions wherever you are.</p>
+        </Container>
+      </Jumbotron>
       <Container>
-        <h1>Your 5 Day Forecast</h1>
-        <p>Showing current weather conditions wherever you are.</p>
+        <CardGroup>
+          <Card>
+            <Card.Img variant="top" src="img/mostly-cloudy.png" />
+            <Card.Body>
+              <Card.Title>Monday</Card.Title>
+              <Card.Text>Mostly Cloudy</Card.Text>
+              <Card.Text>
+                Rain early...then remaining cloudy with showers overnight. Low
+                61F. Winds SW at 5 to 10 mph. Chance of rain 100%. Rainfall near
+                a quarter of an inch.
+              </Card.Text>
+            </Card.Body>
+            <Card.Footer>
+              <small className="text-muted">Last updated 3 mins ago</small>
+            </Card.Footer>
+          </Card>
+          <Card>
+            <Card.Img variant="top" src="img/mostly-cloudy.png" />
+            <Card.Body>
+              <Card.Title>Tuesday</Card.Title>
+              <Card.Text>Mostly Cloudy.</Card.Text>
+              <Card.Text>
+                Rain early...then remaining cloudy with showers overnight. Low
+                61F. Winds SW at 5 to 10 mph. Chance of rain 100%. Rainfall near
+                a quarter of an inch.
+              </Card.Text>
+            </Card.Body>
+            <Card.Footer>
+              <small className="text-muted">Last updated 3 mins ago</small>
+            </Card.Footer>
+          </Card>
+          <Card>
+            <Card.Img variant="top" src="img/mostly-cloudy.png" />
+            <Card.Body>
+              <Card.Title>Wednesday</Card.Title>
+              <Card.Text>Mostly Cloudy</Card.Text>
+              <Card.Text>
+                Rain early...then remaining cloudy with showers overnight. Low
+                61F. Winds SW at 5 to 10 mph. Chance of rain 100%. Rainfall near
+                a quarter of an inch.
+              </Card.Text>
+            </Card.Body>
+            <Card.Footer>
+              <small className="text-muted">Last updated 3 mins ago</small>
+            </Card.Footer>
+          </Card>
+        </CardGroup>
       </Container>
-    </Jumbotron>
+    </>
   );
 };
 
